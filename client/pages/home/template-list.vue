1f62ad28994cf603600d380683d1aa58b7e9db44
gang.a.chen <gang.a.chen@capgemini.com>
2020-04-07 19:01:53 +0800

    Update readme and comments tabs

--- client/pages/home/template-list.vue
+++ client/pages/home/template-list.vue
@@ -4,8 +4,8 @@
       <el-tabs v-model="searchParams.pageMode" @tab-click="handlePageModeClick">
         <el-tab-pane name="h5"><div slot="label"><span class="nav-tabs-label">H5</span></div></el-tab-pane>
         <el-tab-pane name="longPage"><div slot="label"><span class="nav-tabs-label">长图文H5</span></div></el-tab-pane>
-        <el-tab-pane name="relative"><div slot="label"><span class="nav-tabs-label">排版图文</span></div></el-tab-pane>
-        <el-tab-pane name="pc"><div slot="label"><span class="nav-tabs-label">PC页面</span></div></el-tab-pane>
+        <!-- <el-tab-pane name="relative"><div slot="label"><span class="nav-tabs-label">排版图文</span></div></el-tab-pane>
+        <el-tab-pane name="pc"><div slot="label"><span class="nav-tabs-label">PC页面</span></div></el-tab-pane> -->
       </el-tabs>
     </div>
     <div class="page-content">
